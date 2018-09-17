5f86e018cae170fef74e09f0d9da3c05467bd19b
Daniel Schmidt <bigjk55@gmail.com>
2018-09-17 22:44:22 +0200

    added a .xp viewer as example

---
+++ examples/xp-viewer/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+
+	"os"
+
+	"github.com/BigJk/ramen"
+	"github.com/BigJk/ramen/xp"
+)
+
+func main() {
+	filePath := flag.String("file", "", "the REXPaint file to show")
+	flag.Parse()
+
+	if len(*filePath) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	file, err := os.Open(*filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	xp, err := xp.Read(file)
+	if err != nil {
+		panic(err)
+	}
+
+	font, err := ramen.NewFont("../../fonts/terminus-11x11.png", 11, 11)
+	if err != nil {
+		panic(err)
+	}
+
+	con, err := ramen.NewConsole(xp.Width, xp.Height, font, "REXPaint Viewer")
+	if err != nil {
+		panic(err)
+	}
+
+	layers := []*ramen.Console{con}
+	for len(layers) < len(xp.Layers) {
+		newCon, err := layers[len(layers)-1].CreateSubConsole(0, 0, layers[len(layers)-1].Width, layers[len(layers)-1].Height)
+		if err != nil {
+			panic(err)
+		}
+		layers = append(layers, newCon)
+	}
+
+	for l := range xp.Layers {
+		for x := range xp.Layers[l].Cells {
+			for y := range xp.Layers[l].Cells[x] {
+				layers[l].PutCharInt(x, y, xp.Layers[l].Cells[x][y].Char)
+				layers[l].PutColor(x, y, xp.Layers[l].Cells[x][y].Foreground, ramen.ForegroundColor)
+				layers[l].PutColor(x, y, xp.Layers[l].Cells[x][y].Background, ramen.BackgroundColor)
+			}
+		}
+	}
+
+	con.Start(1)
+}
