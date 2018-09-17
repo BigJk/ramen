4b5bcebdf17b4346711052bdf115fae50c2548f0
Daniel Schmidt <bigjk55@gmail.com>
2018-09-17 14:10:12 +0200

    added some fonts and examples

---
+++ example/colored-tiles/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/BigJk/ramen"
+	"github.com/hajimehoshi/ebiten"
+)
+
+func main() {
+	/*
+		load a font that contains colored tiles
+	*/
+	font, err := ramen.NewFont("./custom_font.png", 8, 8)
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		specify which chars in the fonts are colored tiles
+	*/
+	font.SetTiles(272, 16, true)
+
+	/*
+		create a console
+	*/
+	con, err := ramen.NewConsole(40, 20, font, "ramen - colored tiles")
+	if err != nil {
+		panic(err)
+	}
+
+	/*
+		set a pre-render hook
+	*/
+	con.SetPreRenderHook(func(screen *ebiten.Image) error {
+		screen.Fill(color.NRGBA{69, 40, 60, 255})
+
+		/*
+			use colored tiles to draw a frame
+		*/
+		con.PutCharInt(0, 0, 272)
+		con.PutCharInt(1, 0, 276)
+		con.PutCharInt(0, 1, 280)
+
+		con.PutCharInt(con.Width-1, 0, 273)
+		con.PutCharInt(con.Width-2, 0, 277)
+		con.PutCharInt(con.Width-1, 1, 281)
+
+		con.PutCharInt(0, con.Height-1, 274)
+		con.PutCharInt(1, con.Height-1, 278)
+		con.PutCharInt(0, con.Height-2, 282)
+
+		con.PutCharInt(con.Width-1, con.Height-1, 275)
+		con.PutCharInt(con.Width-2, con.Height-1, 279)
+		con.PutCharInt(con.Width-1, con.Height-2, 283)
+
+		/*
+			draw all chars in font
+		*/
+		i := 0
+		for y := 1; y < con.Height-1; y++ {
+			for x := 1; x < con.Width-1; x++ {
+				con.PutCharInt(x, y, i)
+				con.PutColor(x, y, ramen.NewColor(255, 0, 0), ramen.ForegroundColor)
+				i++
+			}
+		}
+		return nil
+	})
+
+	con.Start(2)
+}
---
+++ example/conway/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/BigJk/ramen"
+	"github.com/hajimehoshi/ebiten"
+)
+
+const (
+	Width  = 50
+	Height = 50
+)
+
+var board [][]bool
+
+/*
+	Conway's Game Of Life
+
+	Controls:
+		- Place cell: Left Mouse Button
+		- Step: Space
+*/
+
+func main() {
+	board = createBoard(Width, Height)
+
+	for x := range board {
+		for y := range board[x] {
+			board[x][y] = rand.Intn(5) <= 1
+		}
+	}
+
+	font, err := ramen.NewFont("../../fonts/ti84-6x8.png", 6, 8)
+	if err != nil {
+		panic(err)
+	}
+
+	con, err := ramen.NewConsole(Width, Height, font, "ramen - conway's game of life")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if ebiten.IsKeyPressed(ebiten.KeySpace) {
+				step()
+			}
+
+			if ebiten.IsKeyPressed(ebiten.KeyK) {
+				board = createBoard(Width, Height)
+			}
+
+			for x := range board {
+				for y := range board[x] {
+					if board[x][y] {
+						con.PutColor(x, y, ramen.NewColor(255, 255, 255), ramen.BackgroundColor)
+					} else {
+						con.PutColor(x, y, ramen.NewColor(0, 0, 0), ramen.BackgroundColor)
+					}
+				}
+			}
+
+			if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+				cx, cy := ebiten.CursorPosition()
+
+				bx := cx / font.TileWidth
+				by := cy / font.TileHeight
+
+				board[bx][by] = true
+			} else {
+				time.Sleep(time.Millisecond * 100)
+			}
+		}
+	}()
+
+	con.SetPreRenderHook(func(screen *ebiten.Image) error {
+		return nil
+	})
+
+	con.Start(1)
+}
+
+func createBoard(w, h int) [][]bool {
+	b := make([][]bool, w)
+	for i := range b {
+		b[i] = make([]bool, h)
+	}
+	return b
+}
+
+func getCell(x, y int) bool {
+	wx := x % Width
+	wy := y % Height
+	if wx < 0 {
+		wx += Width
+	}
+	if wy < 0 {
+		wy += Height
+	}
+	return board[wx][wy]
+}
+
+func livingNeighbors(x, y int) int {
+	c := 0
+
+	if getCell(x-1, y) {
+		c++
+	}
+
+	if getCell(x+1, y) {
+		c++
+	}
+
+	if getCell(x, y-1) {
+		c++
+	}
+
+	if getCell(x, y+1) {
+		c++
+	}
+
+	if getCell(x-1, y-1) {
+		c++
+	}
+
+	if getCell(x+1, y+1) {
+		c++
+	}
+
+	if getCell(x-1, y+1) {
+		c++
+	}
+
+	if getCell(x+1, y-1) {
+		c++
+	}
+
+	return c
+}
+
+func step() {
+	nextBoard := createBoard(Width, Height)
+	for x := range board {
+		for y := range board[x] {
+			c := livingNeighbors(x, y)
+			if c < 2 || c > 3 {
+				nextBoard[x][y] = false
+			} else if board[x][y] && (c == 2 || c == 3) {
+				nextBoard[x][y] = true
+			} else if !board[x][y] && c == 3 {
+				nextBoard[x][y] = true
+			}
+		}
+	}
+	board = nextBoard
+}
